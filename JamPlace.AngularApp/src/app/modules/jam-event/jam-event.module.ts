cc57c87ab07ce6e2bedadd7ad6f2cfa328a953a3
Robert Petlak <rpetlak@gmail.com>
2020-03-30 02:50:16 +0200

    Dodano widok dodawania nowego eventu

--- JamPlace.AngularApp/src/app/modules/jam-event/jam-event.module.ts
+++ JamPlace.AngularApp/src/app/modules/jam-event/jam-event.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AddJamEventComponent } from './components/add-jam-event/add-jam-event.component';
 import { JamEventRouterComponent } from './jam-event-router.component';
 import {JamEventRoutingModule} from './jam-event-routing.module'
-
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [AddJamEventComponent, JamEventRouterComponent],
   imports: [
     CommonModule,
-    JamEventRoutingModule
+    JamEventRoutingModule,
+    ReactiveFormsModule
+  
   ]
 })
 export class JamEventModule { }
---
+++ JamPlace.AngularApp/src/app/modules/jam-event/models/addres.ts
@@ -0,0 +1,6 @@
+export class Address {
+    street: string;
+    localNumber: string;
+    city: string;
+    country: string;
+}
