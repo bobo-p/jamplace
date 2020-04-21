b68e54ac493d18a640f7738356e5a2f18aa48103
Robert Petlak <rpetlak@gmail.com>
2020-04-21 20:56:50 +0200

    Zmiana nazwy wyglądu event info panel

--- JamPlace.AngularApp/src/app/modules/jam-event/components/event-info-panel/event-info-panel.component.ts
+++ JamPlace.AngularApp/src/app/modules/jam-event/components/event-info-panel/event-info-panel.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import * as M from "materialize-css/dist/js/materialize";
+
+
 
 @Component({
   selector: 'app-event-info-panel',
   templateUrl: './event-info-panel.component.html',
   styleUrls: ['./event-info-panel.component.scss']
 })
+ 
+
 export class EventInfoPanelComponent implements OnInit {
 
+  @ViewChild('collapsible',{static: false}) elCollapsible: ElementRef;
+  
   constructor() { }
 
   ngOnInit() {
+   
   }
+
+  ngAfterViewInit() {
+    let instanceCollapsible = new M.Collapsible(this.elCollapsible.nativeElement, {});
+}
 
 }
